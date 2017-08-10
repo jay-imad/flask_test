63e04b9f30b66c978241ddf7406dbff9f45cc3ae
Vyakya2 <jay@vyakya.com>
2017-08-10 17:07:19 +0530

    run.py bugs

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
-	from db import db
-	db.init_app(app)
-	app.run(port=5002, debug=True)
+from app import app
+from db import db
+db.init_app(app)
+app.run(port=5002, debug=True)
 
-    @app.before_first_request
-    def create_tables():
-    	db.create_all()
+@app.before_first_request
+def create_tables():
+	db.create_all()
